84c8bfd1902ebf3bb801e8c3b74e5872cb893094
Simeon Georgiev <simeongeorgiev09@gmail.com>
2024-10-16 23:38:27 +0300

    Add assignment

---
+++ src/homework_vectors_problem1.py
@@ -0,0 +1,14 @@
+def calculate_similarity(model, base_word, target_word):
+    pass
+    
+# Пример
+# Вход: model, 'bridge', ['car', 'man', 'arch']
+# Изход: 'arch'
+def find_most_similar_to_given(model, given_word, target_words):
+    pass
+
+# Пример
+# Вход: model, ['lunch', 'breakfast', 'dinner', 'homework']
+# Изход: 'homework'
+def doesnt_match(model, given_words):
+    pass
---
+++ src/homework_vectors_problem2.py
@@ -0,0 +1,7 @@
+# Пример
+# Вход: model, 'man', 'king', 'woman'
+# Изход: 'queen'
+#
+# Пояснение: Връзката между 'man' и 'king' е същата като между 'woman' и 'queen'
+def find_common_meaning(model, base_word, related_word, target_word):
+    pass
